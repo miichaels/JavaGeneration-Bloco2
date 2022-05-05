083cda77df7dae4a3b55f7e62f197ccb403ea4d4
miichaels <michael_araujo2012@hotmail.com>
2022-05-05 17:35:52 -0300

    Projeto Backend de uma Loja de Games com Spring Boot

--- SpringBoot/mundogames/src/main/java/com/games/mundogames/repository/CategoriaRespository.java
+++ SpringBoot/mundogames/src/main/java/com/games/mundogames/repository/CategoriaRespository.java
@@ -3,10 +3,11 @@ package com.games.mundogames.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.games.mundogames.model.CategoriaModel;
 
-
+@Repository
 public interface CategoriaRespository extends JpaRepository<CategoriaModel, Long>{
 	public List<CategoriaModel> findAllByDescricaoIgnoreCase (String descricao);
 }
--- SpringBoot/mundogames/src/main/java/com/games/mundogames/repository/ProdutoRepository.java
+++ SpringBoot/mundogames/src/main/java/com/games/mundogames/repository/ProdutoRepository.java
@@ -3,9 +3,11 @@ package com.games.mundogames.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.games.mundogames.model.ProdutoModel;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>{
 	public List<ProdutoModel> findAllByTituloIgnoreCase(String titulo); // Buscar todos pelo titulo da entidade 
 }
